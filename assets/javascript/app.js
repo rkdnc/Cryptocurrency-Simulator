bb75605fcb4ba198e3d0b3b266d96422bd8dc448
Jeff Nabors <bulletr0k@gmail.com>
2017-12-12 20:26:37 -0500

    Added login system

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,14 +12,54 @@
  var database = firebase.database();
 
  //---------------Login System------------
+ //Getting elements
+ var txtEmail = $("#txtEmail")
+ var txtPassword = $("#txtPassword")
+ var btnLogin = $("#btnLogin");
+ var btnSignUp = $("#btnSignUp");
+ var btnLogout = $("#btnLogout")
+
+ //Login event
+
+ btnLogin.on("click", function (event) {
+     //get email and password
+     var email = txtEmail.val();
+     var password = txtPassword.val();
+     var auth = firebase.auth();
+     //Sign in
+     var promise = auth.signInWithEmailAndPassword(email, password);
+     promise.catch(e => console.log(e.message))
+ });
+
+ //Signup Event
+
+ btnSignUp.on("click", function (event) {
+ //get email and password
+ var email = txtEmail.val();
+ var password = txtPassword.val();
+ var auth = firebase.auth();
+ //Sign in
+ var promise = auth.createUserWithEmailAndPassword(email, password);
+ promise.catch(e => console.log(e.message))
+ });
+
+//Logout user
+btnLogout.on("click", function(event){
+    firebase.auth().signOut();
+})
+
+ //Add realtime listener
+ firebase.auth().onAuthStateChanged(firebaseUser =>{ //Also holy sh*t you can use => for functions
+    if(firebaseUser) {
+        console.log(firebaseUser);
+        btnLogin.css("display", "inline")
+    } else{
+        console.log("not logged in");
+        btnLogin.css("display", "none")
+    }
+ })
  
 
 
 
- //Error handling
- firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-     // Handle Errors here.
-     var errorCode = error.code;
-     var errorMessage = error.message;
-     // ...
- });
+ 
